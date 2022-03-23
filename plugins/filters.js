a3e3369e5f9a4c6af6316311d1b28305a03ad20c
SHAMMASTRICKMAN <102198768+SHAMMASTRICKMAN@users.noreply.github.com>
2022-03-23 14:38:46 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -164,7 +164,7 @@ async function checkImAdmin(message, user = message.client.user.jid) {
         let banned = jid.find( Jid => Jid === message.jid);
         if(banned !== undefined) return
         
-const array = afn','Kooi'
+const array = afn','Kooi']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
